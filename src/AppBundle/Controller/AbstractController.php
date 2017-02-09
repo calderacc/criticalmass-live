69a1ef0f6dff61a91f59e39b6e49ccbb8d7defac
Malte Hübner <maltehuebner@gmx.org>
2017-02-09 20:47:39 +0100

    Chew. Fix typo.

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-abstract class AbstractContoller extends Controller
+abstract class AbstractController extends Controller
 {
 
 }
