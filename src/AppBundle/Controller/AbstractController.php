26676107f14e9e492fb636c51df18336823ac7a3
Malte Hübner <maltehuebner@gmx.org>
2017-02-09 21:28:48 +0100

    Get ride list.

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -3,8 +3,30 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 abstract class AbstractController extends Controller
 {
+    protected function getEntityList(): array
+    {
+        $cache = new FilesystemAdapter();
 
+        $cacheItem = $cache->getItem('ride-list');
+
+        if (!$cacheItem->isHit()) {
+            return [];
+        }
+
+        $jsonList = $cacheItem->get();
+
+        $entityList = [];
+
+        foreach ($jsonList as $json) {
+            /** @var Ride $entity */
+            $entity = $this->get('jms_serializer')->deserialize($json, 'AppBundle\Entity\Ride', 'json');
+            array_unshift($entityList, $entity);
+        }
+
+        return $entityList;
+    }
 }
