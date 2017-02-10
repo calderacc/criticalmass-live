f5299c91bb2a7cd1265dece1c5a4958aebec94fa
Malte Hübner <maltehuebner@gmx.org>
2017-02-10 01:35:35 +0100

    Pack CitySlug in ORM.

--- src/AppBundle/Entity/CitySlug.php
+++ src/AppBundle/Entity/CitySlug.php
@@ -2,24 +2,37 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
 
 /**
+ * @ORM\Entity()
+ * @ORM\Table(name="cityslug")
  * @JMS\ExclusionPolicy("all")
  */
 class CitySlug
 {
     /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      * @JMS\Expose
      * @JMS\Type("integer")
      */
     protected $id;
 
     /**
+     * @ORM\Column(type="string", length=50)
      * @JMS\Expose
      * @JMS\Type("string")
      */
     protected $slug;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="City", inversedBy="slugs")
+     * @ORM\JoinColumn(name="city_id", referencedColumnName="id")
+     */
+    protected $city;
 
     public function getId(): int
     {
@@ -34,6 +47,18 @@ class CitySlug
     public function setSlug(string $slug): CitySlug
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCity(): City
+    {
+        return $this->city;
+    }
+
+    public function setCity(City $city): CitySlug
+    {
+        $this->city = $city;
 
         return $this;
     }
