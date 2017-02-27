720c7804a93b582293015ab91d17646ee0e2f055
Malte Hübner <maltehuebner@gmx.org>
2017-02-27 01:05:51 +0100

    Added stupid api controller.

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class ApiController extends FOSRestController
+{
+    /**
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method"
+     * )
+     */
+    public function getPositionsAction(Request $request)
+    {
+        $view = View::create();
+        $view
+            ->setData(['lala'])
+            ->setFormat('json')
+            ->setStatusCode(200);
+
+        return $this->handleView($view);
+    }
+}
