a1f65b724d1ac4db05dee9e0c109ad3d246e58ac
Malte Hübner <maltehuebner@gmx.org>
2017-02-27 23:01:40 +0100

    Fix many parameter issues.

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Manager\PositionManager;
+use Caldera\GeoBasic\Bounds\Bounds;
+use Caldera\GeoBasic\Coord\Coord;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,14 +19,24 @@ class ApiController extends FOSRestController
      *
      * @ApiDoc(
      *  resource=true,
-     *  description="This is a description of your API method"
+     *  description="This is a description of your API method",
+     *  parameters={
+     *      {"name"="northWestLatitude", "dataType"="float", "required"=true, "description"=""},
+     *      {"name"="northWestLongitude", "dataType"="float", "required"=true, "description"=""},
+     *      {"name"="southEastLatitude", "dataType"="float", "required"=true, "description"=""},
+     *      {"name"="southEastLongitude", "dataType"="float", "required"=true, "description"=""}
+     *  }
      * )
      */
     public function getPositionsAction(Request $request)
     {
+        $bounds = $this->getBoundsFromRequest($request);
+
+        $positionList = $this->getPositionManager()->getPositionsInBounds($bounds);
+
         $view = View::create();
         $view
-            ->setData(['lala'])
+            ->setData($positionList)
             ->setFormat('json')
             ->setStatusCode(200);
 
@@ -34,5 +46,29 @@ class ApiController extends FOSRestController
     protected function getPositionManager(): PositionManager
     {
         return $this->get('criticalmass.manager.incident_manager');
+    }
+
+    protected function getBoundsFromRequest(Request $request): Bounds
+    {
+        if (
+            !$request->query->get('northWestLatitude') ||
+            !$request->query->get('northWestLongitude') ||
+            !$request->query->get('southEastLatitude') ||
+            !$request->query->get('southEastLongitude')
+        ) {
+            throw $this->createNotFoundException('wefwef');
+        }
+
+        $northWest = new Coord(
+            $request->query->get('northWestLatitude'),
+            $request->query->get('northWestLongitude')
+        );
+
+        $southEast = new Coord(
+            $request->query->get('southEastLatitude'),
+            $request->query->get('southEastLongitude')
+        );
+
+        return new Bounds($northWest, $southEast);
     }
 }
