26e323257bc0e5a5370cbf8a55a1e84dc605760c
Malte Hübner <maltehuebner@gmx.org>
2017-02-27 23:49:43 +0100

    Switch config name.

--- src/AppBundle/DependencyInjection/Configuration.php
+++ src/AppBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('caldera_cycleways');
+        $rootNode = $treeBuilder->root('caldera_criticalmasslive');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
