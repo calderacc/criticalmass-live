535587d66ee90678e4d4fdd6f93191fe5ac682ab
Malte Hübner <maltehuebner@gmx.org>
2017-02-27 16:57:54 +0100

    Add type hinting.

--- src/AppBundle/Entity/Position.php
+++ src/AppBundle/Entity/Position.php
@@ -79,139 +79,139 @@ class Position
         $this->creationDateTime = new \DateTime();
     }
 
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function setLatitude($latitude)
+    public function setLatitude(float $latitude): Position
     {
         $this->latitude = $latitude;
 
         return $this;
     }
 
-    public function getLatitude()
+    public function getLatitude(): float
     {
         return $this->latitude;
     }
 
-    public function setLongitude($longitude)
+    public function setLongitude(float $longitude): Position
     {
         $this->longitude = $longitude;
 
         return $this;
     }
 
-    public function getLongitude()
+    public function getLongitude(): float
     {
         return $this->longitude;
     }
 
-    public function setAccuracy($accuracy)
+    public function setAccuracy(float $accuracy = null): Position
     {
         $this->accuracy = $accuracy;
 
         return $this;
     }
 
-    public function getAccuracy()
+    public function getAccuracy(): ?float
     {
         return $this->accuracy;
     }
 
-    public function setAltitude($altitude)
+    public function setAltitude(float $altitude = null): Position
     {
         $this->altitude = $altitude;
 
         return $this;
     }
 
-    public function getAltitude()
+    public function getAltitude(): ?float
     {
         return $this->altitude;
     }
 
-    public function setAltitudeAccuracy($altitudeAccuracy)
+    public function setAltitudeAccuracy(float $altitudeAccuracy): Position
     {
         $this->altitudeAccuracy = $altitudeAccuracy;
 
         return $this;
     }
 
-    public function getAltitudeAccuracy()
+    public function getAltitudeAccuracy(): ?float
     {
         return $this->altitudeAccuracy;
     }
 
-    public function setHeading($heading)
+    public function setHeading(float $heading = null): Position
     {
         $this->heading = $heading;
 
         return $this;
     }
 
-    public function getHeading()
+    public function getHeading(): float
     {
         return $this->heading;
     }
 
-    public function setSpeed($speed)
+    public function setSpeed(float $speed = null): Position
     {
         $this->speed = $speed;
 
         return $this;
     }
 
-    public function getSpeed()
+    public function getSpeed(): ?float
     {
         return $this->speed;
     }
 
-    public function setTimestamp($timestamp)
+    public function setTimestamp(int $timestamp = null): Position
     {
         $this->timestamp = $timestamp;
 
         return $this;
     }
 
-    public function getTimestamp()
+    public function getTimestamp(): int
     {
         return $this->timestamp;
     }
 
-    public function setCreationDateTime($creationDateTime)
+    public function setCreationDateTime(\DateTime $creationDateTime = null): Position
     {
         $this->creationDateTime = $creationDateTime;
 
         return $this;
     }
 
-    public function getCreationDateTime()
+    public function getCreationDateTime(): ?\DateTime
     {
         return $this->creationDateTime;
     }
 
-    public function setGlympseTicket(GlympseTicket $glympseTicket = null)
+    public function setGlympseTicket(GlympseTicket $glympseTicket = null): Position
     {
         $this->glympseTicket = $glympseTicket;
 
         return $this;
     }
 
-    public function getGlympseTicket()
+    public function getGlympseTicket(): ?GlympseTicket
     {
         return $this->glympseTicket;
     }
 
-    public function setCriticalmapsUser(CriticalmapsUser $criticalmapsUser)
+    public function setCriticalmapsUser(CriticalmapsUser $criticalmapsUser): Position
     {
         $this->criticalmapsUser = $criticalmapsUser;
 
         return $this;
     }
 
-    public function getCriticalmapsUser()
+    public function getCriticalmapsUser(): ?CriticalmapsUser
     {
         return $this->criticalmapsUser;
     }
