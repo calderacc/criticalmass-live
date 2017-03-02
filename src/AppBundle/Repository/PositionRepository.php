20f110267ff98352f9bf297227bdff6edff95d4a
Malte Hübner <maltehuebner@gmx.org>
2017-03-02 16:50:03 +0100

    Restrict position query to last 5 minutes.

--- src/AppBundle/Repository/PositionRepository.php
+++ src/AppBundle/Repository/PositionRepository.php
@@ -7,13 +7,19 @@ use Doctrine\ORM\EntityRepository;
 
 class PositionRepository extends EntityRepository
 {
-    public function findCurrentPositions(): array
+    public function findCurrentPositions(int $age = 5): array
     {
+        $interval = new \DateInterval('PT' . $age . 'M');
+        $dateTime = new \DateTime();
+        $dateTime->sub($interval);
+
         $qb = $this->createQueryBuilder('p');
 
         $qb
+            ->where($qb->expr()->gte('p.creationDateTime', ':dateTime'))
             ->addGroupBy('p.glympseTicket')
             ->addGroupBy('p.criticalmapsUser')
+            ->setParameter('dateTime', $dateTime->format('Y-m-d H:i:s'))
         ;
 
         $query = $qb->getQuery();
