1e93328af276a91606beccadf586e1b1f7034df3
Malte Hübner <maltehuebner@gmx.org>
2017-04-27 00:09:22 +0200

    Workaround for broken slugs.

--- src/AppBundle/Command/RefreshRideListCommand.php
+++ src/AppBundle/Command/RefreshRideListCommand.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\City;
+use AppBundle\Entity\Ride;
 use Curl\Curl;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -45,7 +47,10 @@ class RefreshRideListCommand extends ContainerAwareCommand
         $progress->start();
 
         foreach ($rideList as $json) {
+            /** @var Ride $ride */
             $ride = $serializer->deserialize(json_encode($json), 'AppBundle\Entity\Ride', 'json');
+
+            $this->fixDoubleSlugObjects($ride->getCity());
 
             if ($manager->getUnitOfWork()->isEntityScheduled($ride)) {
                 $manager->merge($ride);
@@ -57,5 +62,30 @@ class RefreshRideListCommand extends ContainerAwareCommand
         }
 
         $manager->flush();
+    }
+
+    /**
+     * Yes, it is a fucking workaround.
+     *
+     * When jms deserializes, $city->mainSlug can contain an CitySlug object which is already going to persisted in
+     * $city->slugs array. Doctrine does not understand the main slug to be one element of the slug list so it tries
+     * to persist to slug objects which an identical id and fails.
+     *
+     * We save the id of the main slug here and replace it with one from the slug list so it is the same reference to
+     * the same object.
+     */
+    protected function fixDoubleSlugObjects(City $city): void
+    {
+        $mainSlugId = $city->getMainSlug()->getId();
+
+        $city->setMainSlug(null);
+
+        foreach ($city->getSlugs() as $slug) {
+            if ($slug->getId() === $mainSlugId) {
+                $city->setMainSlug($slug);
+
+                break;
+            }
+        }
     }
 }
